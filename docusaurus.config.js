1df3d57e4f87934c9fab3c5c1ff27d3024814571
Jérémie Colmant <jecolmant@gmail.com>
2024-08-07 09:49:48 +0200

    first commit

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -147,4 +147,6 @@ const config = {
     }),
 };
 
+
+
 export default config;
---
+++ src/components/HomepageFeatures/MyCustomTip.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import styles from './MyCustomTip.module.css'; // Fichier CSS pour les styles
+
+function MyCustomTip({ children }) {
+  return (
+    <div className={styles.myTip}>
+      <div className={styles.myTipTitle}></div> {/* Titre */}
+      <div className={styles.myTipContent}>{children}</div> {/* Contenu */}
+    </div>
+  );
+}
+
+export default MyCustomTip;
+
