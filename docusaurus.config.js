65a51678649979b0765829a1b95097eef9c77e9d
Jérémie Colmant <jecolmant@gmail.com>
2024-08-09 11:32:31 +0200

    first commit

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -18,7 +18,7 @@ const config = {
   onBrokenMarkdownLinks: 'warn',
   i18n: {
     defaultLocale: 'en',
-    locales: ['en'],
+    locales: ['en']
   },
 
   presets: [
--- src/components/HomepageFeatures/MyCustomTip.js
+++ src/components/HomepageFeatures/MyCustomTip.js
@@ -12,3 +12,4 @@ function MyCustomTip({ children }) {
 
 export default MyCustomTip;
 
+
